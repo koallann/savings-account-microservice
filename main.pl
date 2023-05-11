9d645671bcced22bc33fbb0e0af72245a87d97ce
Allan Lima <allancslima@gmail.com>
2023-05-11 06:13:35 -0300

    Handling each server request on a new thread

--- main.pl
+++ main.pl
@@ -4,6 +4,8 @@ use strict;
 
 $| = 1;
 
+use threads;
+
 use HTTP::Daemon;
 use HTTP::Response;
 use HTTP::Status;
@@ -24,49 +26,54 @@ my $server = HTTP::Daemon->new(
     LocalAddr => "127.0.0.1",
     LocalPort => 8000,
     ReuseAddr => 1,
-) or die;
+) or die "Error creating server!";
 
 # running server
 
 print "Running server on ", $server->url, "\n";
 
 while (my $conn = $server->accept()) {
-    while (my $req = $conn->get_request()) {
-        # allow GET only
-        my $method = $req->method;
-        if ($method ne "GET") {
-            my $res = create_bad_response("Method not allowed.");
-            $conn->send_response($res);
-            next;
-        }
+    threads->create(\&handle_connection, $conn);
+}
 
-        # allow path / only
-        my $path = $req->uri->path;
-        if ($path ne "/") {
-            my $res = create_bad_response("Path not allowed.");
-            $conn->send_response($res);
-            next;
-        }
+sub handle_connection {
+    my ($conn) = @_;
+    my $res = handle_request($conn->get_request());
 
-        # unmarshal request body
-        my $body = decode_json($req->content);
-
-        # check type field
-        my $type = $body->{"type"};
-        if ($type ne TYPE_SERVER) {
-            my $res = create_bad_response("The 'type' field doesn't match this service.");
-            $conn->send_response($res);
-            next;
-        }
-
-        # handle action and respond
-        my $action = $body->{"action"};
-        my $content = $body->{"content"};
-
-        my $res = handle_action($action, $content);
-        $conn->send_response($res);
-    }
+    $conn->send_response($res);
     $conn->close();
+}
+
+sub handle_request {
+    my ($req) = @_;
+    sleep(5);
+
+    # allow GET only
+    my $method = $req->method;
+    if ($method ne "GET") {
+        return create_bad_response("Method not allowed.");
+    }
+
+    # allow path / only
+    my $path = $req->uri->path;
+    if ($path ne "/") {
+        return create_bad_response("Path not allowed.");
+    }
+
+    # unmarshal request body
+    my $body = decode_json($req->content);
+
+    # check type field
+    my $type = $body->{"type"};
+    if ($type ne TYPE_SERVER) {
+        return create_bad_response("The 'type' field doesn't match this service.");
+    }
+
+    # handle action
+    my $action = $body->{"action"};
+    my $content = $body->{"content"};
+
+    return handle_action($action, $content);
 }
 
 sub handle_action {
@@ -86,12 +93,12 @@ sub handle_action {
 }
 
 sub create_bad_response {
-    my ($body) = @_;
+    my ($message) = @_;
 
     return HTTP::Response->new(
         RC_BAD_REQUEST,
         status_message(RC_BAD_REQUEST),
         ["Content-Type" => "application/json"],
-        encode_json({"message" => $body}),
+        encode_json({"message" => $message}),
     );
 }
